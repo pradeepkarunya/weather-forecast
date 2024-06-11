1b88992293243437256d2aabc8bb7a9c3e07cf04
Pradeep Kumar Sundaramurthy <pradeepkarunya@gmail.com>
2024-06-11 15:27:07 +0530

    Azure Test2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather Updates.......11
+      Welcome to Weather Updates
       <WeatherUpdates />
     </div>
   );
